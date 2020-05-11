c7e3b985f7cce1e9f54fc37518a6a6b0deefc84b
mignatyeva <margarita.ignatyeva@nctechimaging.com>
2020-05-11 20:59:45 +0100

    async await for reading data files

--- src/service/service.js
+++ src/service/service.js
@@ -2,32 +2,33 @@
 
 const version = require(`./version`);
 const help = require(`./help`);
-const dummyDataCenter = require(`./data`);
-const cliArgs = process.argv.slice(2);
+const generate = require(`./generate`);
 const errors = require(`../errorMessages`);
 
 const customCommands = [{
-  name: `version`,
+  name: `--version`,
   action: version.getVersion,
   description: `Outputs the version`
 }, {
-  name: `help`,
+  name: `--help`,
   action: help.printInfo,
   description: `Shows the available commands`
 }, {
-  name: `generate`,
-  action: (count) => dummyDataCenter.generateMocks(count),
+  name: `--generate`,
+  action: (count) => generate.run(count),
   description: `Outputs the version`
 }];
 
-const getCommand = (commandName) => customCommands.find((command) => command.name === commandName) || null;
+const [
+  commandName,
+  ...params
+] = process.argv.slice(2);
 
-const parameters = cliArgs.join(` `).split(`--`).filter((item) => item);
-parameters.forEach((item) => {
-  const splitCommand = item.split(` `);
-  const customCommand = getCommand(splitCommand[0]);
-  if (customCommand) {
-    return customCommand.action(splitCommand[1]);
-  }
-  return errors.commandUnknown(item);
-});
+const command = customCommands.find(command => command.name === commandName);
+
+if (command) {
+  command.action(...params);
+} else {
+  errors.commandUnknown(commandName);
+};
+
