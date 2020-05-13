4b4f685bd1b647efbeeaa9510be1393a2ac5dabf
mignatyeva <margarita.ignatyeva@nctechimaging.com>
2020-05-13 21:24:48 +0100

    cleanup

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const MAX_LIMIT = 1000;
+
+module.exports = {
+  MAX_LIMIT
+};
--- src/service/showVersion.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const {version} = require(`../../package.json`);
-const getVersion = () => console.log(version);
-module.exports = {
-  getVersion
-};
-
