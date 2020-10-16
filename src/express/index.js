971d404323712181b157af86ccf1569e20ee3027
mignatyeva <margarita.ignatyeva@nctechimaging.com>
2020-10-16 19:25:47 +0100

    add static path for public folder

--- src/express/index.js
+++ src/express/index.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.set(`views`, path.join(__dirname, `./templates`));
 app.set(`view engine`, `pug`);
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get(`/offers`, (req, res) => {
   res.render(`main--empty`);
